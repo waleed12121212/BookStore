be707e642e000e548907bcba6eae8f7358f32a1d
waleed <120984229+waleed12121212@users.noreply.github.com>
2024-05-18 11:48:40 +0300

    Add project files.

---
+++ BookStore/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [MaxLength(30)]
+        public string Name { get; set; } = null!;//? the same
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public DateTime UpdatedOn { get; set; } = DateTime.Now;
+
+        /* Category( )
+         {
+             CreatedOn = DateTime.Now;
+         }*/
+    }
+}
